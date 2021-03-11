f78f07d9bd4e518d10f23131a73f1293dfa0ed95
juan zabala <juanzabaladev@gmail.com>
2021-03-10 21:13:26 -0500

    Se agrega etiqueta select para  selecccionar país y categoría y hacer consulta a la api de noticias

--- repaso_javascript/app.js
+++ repaso_javascript/app.js
@@ -1,15 +1,17 @@
-const fetchData = async () => {
-    let response = await fetch("http://newsapi.org/v2/top-headlines?country=co&category=business&apiKey=f666b1a15c8940dc9df48ce0596933fa");
-    let dataJson = await response.json();
-    return dataJson.articles;
+const fetchData = async (country="co", category="business") => {
+  let response = await fetch(`http://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=f666b1a15c8940dc9df48ce0596933fa`);
+  let dataJson = await response.json();
+  return dataJson.articles;
 }
-const renderData = async () => {
-    const articles = await fetchData();
-    let container = document.getElementById("articles");
-    let urlImageNotFound = "https://programacion.net/files/article/20160819020822_image-not-found.png";
-    // conticion ? instrucción : intrcción
-    articles.forEach((article) => {
-        let alertTemplate = `<div class="col-12 col-sm-12 col-md-6 col-lg-4">
+const renderData = async (category) => {
+  let country = document.getElementById("countries").value;
+  const articles = await fetchData(country, category);
+  let container = document.getElementById("articles");
+  container.innerHTML = "";
+  let urlImageNotFound = "https://programacion.net/files/article/20160819020822_image-not-found.png";
+  // conticion ? instrucción : intrcción
+  articles.forEach((article) => {
+    let alertTemplate = `<div class="col-12 col-sm-12 col-md-6 col-lg-4">
         <div class="card" style="width: 18rem;">
             <img src="${article.urlToImage == null ? urlImageNotFound : article.urlToImage}"class="card-img-top" alt="...">
             <div class="card-body">
@@ -18,8 +20,8 @@ const renderData = async () => {
             </div>
           </div>
         </div>`;
-        container.innerHTML += alertTemplate;
-    });
+    container.innerHTML += alertTemplate;
+  });
 }
 renderData();
 
