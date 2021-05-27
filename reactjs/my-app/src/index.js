6c9301c749ac810ed5f5e643d6f5be6891b5233f
juan zabala <juanzabaladev@gmail.com>
2021-05-26 20:25:46 -0500

    creacion de cmponentes en reactjs y se agrega materialize como framwrok css

--- reactjs/my-app/src/index.js
+++ reactjs/my-app/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import LoginPage  from './login/LoginPage';
+import HeaderComponent from './shared/components/header/HeaderComponent';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HeaderComponent />
+    <LoginPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ reactjs/my-app/src/login/LoginPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './LoginStyles.css';
+function LoginPage() {
+    return (
+        <div className="row">
+            <form className="col s12">
+                <div className="row">
+                    <div className="input-field col s6">
+                        <i className="material-icons prefix">account_circle</i>
+                        <input id="icon_prefix" type="text" className="validate" />
+                        <label for="icon_prefix">First Name</label>
+                    </div>
+                    <div className="input-field col s6">
+                        <i className="material-icons prefix">phone</i>
+                        <input id="icon_telephone" type="tel" className="validate" />
+                        <label for="icon_telephone">Telephone</label>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
+export default LoginPage;
